3f324b9960f69b730590b9a3036839536b9667a5
Shafty89 <oula_89@hotmail.com>
2020-07-21 22:04:09 +0300

    Introduced Weapon zoom + added some walls

---
+++ Assets/Scripts/WeaponZoom.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WeaponZoom : MonoBehaviour
+{
+
+    [SerializeField] Camera fpsCam;
+    [SerializeField] float zoomedOutFOV = 60f;
+    [SerializeField] float zoomedInFOV = 20f;
+
+    bool zoomToggle = false;
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
+        {
+            if (zoomToggle == false)
+            {
+                zoomToggle = true;
+                fpsCam.fieldOfView = zoomedInFOV;
+            }
+            else
+            {
+                zoomToggle = false;
+                fpsCam.fieldOfView = zoomedOutFOV;
+            }
+        }
+    }
+
+}
