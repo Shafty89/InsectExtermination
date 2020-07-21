c9a5b37906132cfaa4ca50df2b4fe51045af0ae6
Shafty89 <oula_89@hotmail.com>
2020-07-21 22:17:49 +0300

    Fixed sensitivity values

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.FirstPerson;
 
 public class WeaponZoom : MonoBehaviour
 {
@@ -8,8 +9,17 @@ public class WeaponZoom : MonoBehaviour
     [SerializeField] Camera fpsCam;
     [SerializeField] float zoomedOutFOV = 60f;
     [SerializeField] float zoomedInFOV = 20f;
+    [SerializeField] float zoomOutSensitivity = 2f;
+    [SerializeField] float zoomInSensitivity = .7f;
+
+    RigidbodyFirstPersonController fpsController;
 
     bool zoomToggle = false;
+
+    private void Start()
+    {
+        fpsController = GetComponent<RigidbodyFirstPersonController>();
+    }
 
     private void Update()
     {
@@ -19,11 +29,15 @@ public class WeaponZoom : MonoBehaviour
             {
                 zoomToggle = true;
                 fpsCam.fieldOfView = zoomedInFOV;
+                fpsController.mouseLook.XSensitivity = zoomInSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomInSensitivity;
             }
             else
             {
                 zoomToggle = false;
                 fpsCam.fieldOfView = zoomedOutFOV;
+                fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
+                fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
             }
         }
     }
