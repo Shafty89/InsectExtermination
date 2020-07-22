53fabf04f89bcd7eae1f514fb604d2476c930083
Shafty89 <oula_89@hotmail.com>
2020-07-22 13:52:23 +0300

    Zoom bug fixed when changing weapons

--- Assets/Scripts/WeaponZoom.cs
+++ Assets/Scripts/WeaponZoom.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
@@ -16,6 +17,11 @@ public class WeaponZoom : MonoBehaviour
 
     bool zoomToggle = false;
 
+    private void OnDisable()
+    {
+        ZoomOut();
+    }
+
 
     private void Update()
     {
@@ -23,19 +29,29 @@ public class WeaponZoom : MonoBehaviour
         {
             if (zoomToggle == false)
             {
-                zoomToggle = true;
-                fpsCam.fieldOfView = zoomedInFOV;
-                fpsController.mouseLook.XSensitivity = zoomInSensitivity;
-                fpsController.mouseLook.YSensitivity = zoomInSensitivity;
+                zoomIn();
             }
+
             else
             {
-                zoomToggle = false;
-                fpsCam.fieldOfView = zoomedOutFOV;
-                fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
-                fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
+                ZoomOut();
             }
         }
     }
 
+    private void zoomIn()
+    {
+        zoomToggle = true;
+        fpsCam.fieldOfView = zoomedInFOV;
+        fpsController.mouseLook.XSensitivity = zoomInSensitivity;
+        fpsController.mouseLook.YSensitivity = zoomInSensitivity;
+    }
+
+    private void ZoomOut()
+    {
+        zoomToggle = false;
+        fpsCam.fieldOfView = zoomedOutFOV;
+        fpsController.mouseLook.XSensitivity = zoomOutSensitivity;
+        fpsController.mouseLook.YSensitivity = zoomOutSensitivity;
+    }
 }
