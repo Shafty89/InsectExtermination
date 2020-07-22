9639bcc434e51d6571a7c1daee93b95c1e41e065
Shafty89 <oula_89@hotmail.com>
2020-07-22 23:16:08 +0300

    Added battery pickups

---
+++ Assets/Scripts/BatteryPickups.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BatteryPickups : MonoBehaviour
+{
+    [SerializeField] float restoreAngle = 90f;
+    [SerializeField] float addIntensity = 1f;
+
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            other.GetComponentInChildren<FlashLight>().RestoreLightAngle(restoreAngle);
+            other.GetComponentInChildren<FlashLight>().AddLightIntensity(addIntensity);
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/Scripts/FlashLight.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLight : MonoBehaviour
+{
+    [SerializeField] float lightDecay = .1f;
+    [SerializeField] float angleDecay = 1f;
+    [SerializeField] float minimumAngle = 40f;
+
+    Light playerLight;
+
+    private void Start()
+    {
+        playerLight = GetComponent<Light>();
+    }
+
+    private void Update()
+    {
+        DecreaseLightAngle();
+        DecreaseLightIntensity();
+    }
+
+    public void RestoreLightAngle(float restoreAngle)
+    {
+        playerLight.spotAngle = restoreAngle;
+    }
+    public void AddLightIntensity(float addIntensity)
+    {
+        playerLight.intensity += addIntensity;
+    }
+
+    private void DecreaseLightIntensity()
+    {
+        playerLight.intensity -= lightDecay * Time.deltaTime;
+    }
+
+    private void DecreaseLightAngle()
+    {
+        if (playerLight.spotAngle <= minimumAngle)
+        {
+            return;
+        } else
+        {
+            playerLight.spotAngle -= angleDecay * Time.deltaTime;
+        }
+        
+    }
+}
